0a0ac6550d8c0b8ab866bded924245f8917f8796
willianrr <willianrogerioregina@gmail.com>
2024-10-03 19:52:10 -0300

    add: first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col overflow-x-hidden font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <div className="p-4">Olá mundo</div>
+      </main>
+    </div>
+  )
+}
